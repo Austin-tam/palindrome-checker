826e74a47c3656a6ccf091868d352709ed800775
Austin Tam <austin.tam7@gmail.com>
2020-11-25 14:12:10 -0500

    add background color change indication

--- js/main.js
+++ js/main.js
@@ -6,11 +6,13 @@ function palindrome() {
 
   if (str.length === 1){
     document.getElementById("palindrome-label").innerText = "\"" + str + "\" is a palindrome.";
+    document.getElementById("main").style.backgroundColor = "#09ad00";
     return;
   }
 
   if (str.length === 0){
-    document.getElementById("palindrome-label").innerText = "Check your palindrome (Non-alphanumeric characters will be ignored)"
+    document.getElementById("palindrome-label").innerText = "Check your palindrome (Non-alphanumeric characters will be ignored)";
+    document.getElementById("main").style.backgroundColor = "#e1ad01";
     return;
   }
 
@@ -19,9 +21,11 @@ function palindrome() {
   for (let x = 0; x < parseInt(str.length/2); x++) {
     if (str[x] !== str[str.length - 1 - x]) {
       document.getElementById("palindrome-label").innerText = "\"" + document.getElementById("palindrome-input").value + "\" is not a palindrome.";
+      document.getElementById("main").style.backgroundColor = "#ba0000";
       return;
     }
   }
 
   document.getElementById("palindrome-label").innerText = "\"" + document.getElementById("palindrome-input").value + "\" is a palindrome.";
+  document.getElementById("main").style.backgroundColor = "#09ad00";
 };
